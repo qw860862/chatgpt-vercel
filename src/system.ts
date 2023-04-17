44071083c67e3af5008795d5ac7c0e5c49e76c0f
qw860862 <101037371+qw860862@users.noreply.github.com>
2023-04-17 18:26:10 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -10,7 +10,7 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `Powered by Bejing Zhenzhigang`
+export const defaultMessage = `Powered by Bejing Zhenzhigang
 - 人生助手机器人项目，由 [北京真知港网络技术中心](https://www.bjzzg.com)开发，版权所有。
 - 操作技巧：
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
